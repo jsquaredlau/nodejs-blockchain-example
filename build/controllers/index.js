2d95ab834f3470b31c2fedff951ca44916a5c668
JJ Lau <j.j.j.lau@live.com>
2017-01-28 01:00:34 +1100

    Node w/ ts + npm typing package

---
+++ build/controllers/index.js
@@ -0,0 +1,6 @@
+"use strict";
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+/* app/controllers/index.ts */
+__export(require("./welcomeController"));
