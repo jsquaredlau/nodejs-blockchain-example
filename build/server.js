c227430b69467f5aae81d97e2494f3b6d8df7ac3
JJ Lau <j.j.j.lau@live.com>
2017-01-28 21:01:24 +1100

    Removed build folder

--- build/server.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var express = require("express");
-var controllers_1 = require("./controllers");
-var app = express();
-var port = process.env.PORT || 3000;
-app.use('/welcome', controllers_1.WelcomeController);
-app.listen(port, function () {
-    console.log("Listening at http://localhost:" + port + "/");
-});
